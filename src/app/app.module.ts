38dd4f06c8ebd0a0cba9a3214a1fc6b3a4383b37
esraa1711 <esraakhaled163@gmail.com>
2023-02-23 22:09:44 +0200

    Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavbarComponent,
+    FooterComponent,
+
+    ProductListComponent,
+      ProductCardComponent,
+      ProductDetailsComponent,
+    
   ],
   imports: [
     BrowserModule,
